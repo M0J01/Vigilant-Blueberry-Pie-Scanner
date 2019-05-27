89320f421e16b75c21255d69b675044febd5a158
John Moody <m0j01@Johns-MacBook-Pro.local>
2019-05-27 01:02:17 -0400

    Updated code a little

--- Main.py
+++ Main.py
@@ -2,12 +2,12 @@ import pexpect
 
 results = open("scan_results.txt", 'w')
 child = pexpect.spawn("bluetoothctl")
-child.send("scan on \n")
+child.send("scan on\n")
 bdaddrs = []
 
 try:
     while True:
-        child.expect("Device (([0-9A-Fa-f]{2}:)[{5}(0-9A-Fa-f]{2}))")
+        child.expect("Device (([0-9A-Fa-f]{2}:){5}([0-9A-Fa-f]{2}))")
         bdaddr = child.match.group(1)
         if bdaddr not in bdaddrs:
             bdaddrs.append(bdaddr)
